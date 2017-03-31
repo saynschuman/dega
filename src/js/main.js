5be11450d27fe8719bc62fa66087bb04ac123cb6
saynschuman <saynschuman@gmail.com>
2017-03-31 19:17:26 +0300

    desktop finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,16 @@
 (function ($) {
 
+$('.slider').slick({
+	arrows: false,
+	fade: true,
+  	cssEase: 'linear',
+  	autoplay: true,
+  	autoplaySpeed: 2500
+})
 
+$('.specialists').slick({
+	prevArrow: '<div class="prev"></div>',
+	nextArrow: '<div class="next"></div>'
+});
 
 }(jQuery));
